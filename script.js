e4bfce1b7a28c09502fa9a94bc08cbf331750fe2
wayan2602 <23402010004@student.prasetiyamulya.ac.id>
2023-01-23 00:41:01 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Toggle Class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+// when click
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Click outside slidebar
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
